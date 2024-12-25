4511b7e98f1f0cd87d0f6b419bb4db7ac447a8d8
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-12-25 13:55:06 +0200

    feat: add non generic add configuration method & unit tests (#6)

--- Microsoft.Configuration.Extensions/ConfigurationExtensions.cs
+++ Microsoft.Configuration.Extensions/ConfigurationExtensions.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -5,6 +6,14 @@ namespace Microsoft.Configuration.Extensions;
 
 public static class ConfigurationExtensions
 {
+    private static readonly MethodInfo AddConfigurationGenericMethod = typeof(ConfigurationExtensions).GetMethod(
+        "AddConfiguration",
+        BindingFlags.Static | BindingFlags.Public,
+        null,
+        [typeof(IServiceCollection), typeof(IConfiguration), typeof(string)],
+        null
+    )!;
+
     public static IServiceCollection AddConfiguration<T>(
         this IServiceCollection services,
         IConfiguration configuration,
@@ -12,9 +21,20 @@ public static class ConfigurationExtensions
     )
         where T : class => services.Configure<T>(configuration.GetSection(name ?? typeof(T).Name));
 
+    public static IServiceCollection AddConfiguration(
+        this IServiceCollection services,
+        Type type,
+        IConfiguration configuration,
+        string? name = null
+    )
+    {
+        var method = AddConfigurationGenericMethod.MakeGenericMethod(type);
+        return (IServiceCollection)method.Invoke(null, [services, configuration, name]);
+    }
+
     public static T? GetSectionAs<T>(this IConfiguration configuration, string? sectionName = null) =>
         configuration.GetSection(sectionName ?? typeof(T).Name).Get<T>();
 
-    public static object? GetSectionByType(this IConfiguration configuration, Type type, string sectionName) =>
-        configuration.GetSection(sectionName).Get(type);
+    public static object? GetSectionByType(this IConfiguration configuration, Type type, string? sectionName = null) =>
+        configuration.GetSection(sectionName ?? type.Name).Get(type);
 }
